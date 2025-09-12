1453ee47be3a5d5d5aee7607b31fe0c143606e02
Maël Kerbiriou <m431.kerbiriou@gmail.com>
2025-09-12 22:51:08 +0200

    fixup xdpmem

--- src/xdpmem.h
+++ src/xdpmem.h
@@ -1,6 +1,8 @@
 #ifndef xdpmem_h
 #define xdpmem_h
 
+#include "arrays.h"
+
 
 static const float MINUS_INFINITY = -9e9f;
 static const float UNINIT = -8e8f;
@@ -45,7 +47,6 @@ public:
 
 	void Clear()
 		{
-		m_TBBit = {};
 		myfree(m_Buffer1);
 		myfree(m_Buffer2);
 		myfree(m_Buffer1_Int);
